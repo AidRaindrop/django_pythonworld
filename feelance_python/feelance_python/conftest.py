00d5d302eb650ef5ff743a8e4dec6afc05fc41c6
Burak Eduard <burak.edouard@yandex.by>
2023-05-09 17:52:40 +0300

    Initial commit

---
+++ feelance_python/feelance_python/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from feelance_python.users.models import User
+from feelance_python.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
